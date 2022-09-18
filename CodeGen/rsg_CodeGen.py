b86e28389fb000ee596453341b980c4d2eebf479
Ulrik Lindahl <ulrik@ulriklindahl.com>
2022-09-18 10:27:56 +0200

    * Updated pds * Removed vs project files

--- CodeGen/rsg_CodeGen.py
+++ CodeGen/rsg_CodeGen.py
@@ -1,18 +1,21 @@
 # relentless-sg - Persistent data scene graph. Copyright (c) 2022 Ulrik Lindahl
 # Licensed under the MIT license https://github.com/Cooolrik/relentless-sg/blob/main/LICENSE
 
+# add an import path to the code generator in pds
 import sys
 sys.path.append('../Dependencies/persistent-ds/CodeGen')
 
+# import the code generators
 import CodeGeneratorHelpers as hlp
 from EntitiesHelpers import *
 
+# set up all items in rsg
 Items = []
-
 import Nodes.NodeTransform 
 import Nodes.NodeGeometry
 import Scene.Scene 
 import Scene.SceneLayer 
 import Geometry.Mesh
 
+# run the generator
 hlp.run_module('EntityGenerator', "../Include/rsg" ,  "../Src" , Items)
